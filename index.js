5cd0b8c0e4e7acd4d5d4a66048645a28f31fea05
Itamar1234 <Itamardarihess@gmail.com>
2023-03-15 23:25:47 +0200

    styling

--- index.js
+++ index.js
@@ -5,13 +5,6 @@ let qr_generateor = new QRCode(qr_code_element);
 socket.emit("get_qr_code");
 
 socket.on("qr_code", qr => {
-    if (qr === "not available") {
-        setTimeout(() => {
-            socket.emit("get_qr_code");
-        }, 4000);
-    } else {
-        qr = qr.padEnd(220);
-        qr_generateor.makeCode(qr);
-    }
-    console.log(qr);
+    qr = qr.padEnd(220);
+    qr_generateor.makeCode(qr);
 });
