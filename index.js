17bff034f8d147904a7a371314663c473f0a7d5e
Itamar1234 <Itamardarihess@gmail.com>
2023-03-16 23:04:34 +0200

    design changes

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const socket = io("http://localhost:3000");
 const qr_code_element = document.querySelector(".qr-code");
 let qr_generateor = new QRCode(qr_code_element);
+const before_login = el(".before-login");
+const spiner = el(".spiner");
+const check_block = el(".check-block");
+const login_check_box = el(".login-check-box");
+
+
 
 socket.emit("get_qr_code");
 
@@ -8,3 +14,38 @@ socket.on("qr_code", qr => {
     qr = qr.padEnd(220);
     qr_generateor.makeCode(qr);
 });
+
+socket.on("loged in", () => {
+    success_login();
+});
+
+
+function el(el) {
+    return document.querySelector(el)
+}
+
+function success_login() {
+    before_login.style.opacity = "0";
+    success_popup();
+    setTimeout(() => {
+        start_success_animation();
+    }, 500);
+}
+
+
+function start_success_animation() {
+    spiner.style.animationPlayState = "running";
+    setTimeout(() => {
+        check_block.style.animationPlayState = "running";
+    }, 1000);
+}
+
+function success_popup() {
+    login_check_box.style.transform = "translate(-50%,-50%) scale(1.2)";
+    setTimeout(() => {
+        login_check_box.style.transform = "translate(-50%,-50%) scale(1)";
+    }, 200);
+}
+setTimeout(() => {
+    success_login();
+}, 1000);
